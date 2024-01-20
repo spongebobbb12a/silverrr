27dec262671775edbd3443463b3ca02dbe1b45c2
spongebobbb12a <153310734+spongebobbb12a@users.noreply.github.com>
2024-01-20 18:11:40 +0800

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -35,7 +35,7 @@ async def channel_post(client: Client, message: Message):
         print(e)
         await reply_text.edit_text("<b>Telah Terjadi Error...</b>")
         return
-    converted_id = post_message.message_id * abs(client.db_channel.id)
+    converted_id = post_message.message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
@@ -61,14 +61,13 @@ async def channel_post(client: Client, message: Message):
 
 
 @Bot.on_message(
-    filters.channel & filters.incoming & filters.chat(CHANNEL_ID) & ~filters.edited
-)
+    @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID)
 async def new_post(client: Client, message: Message):
 
     if DISABLE_CHANNEL_BUTTON:
         return
 
-    converted_id = message.message_id * abs(client.db_channel.id)
+    converted_id = message.message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
